f926635afed369b566a422170313f29d70a9ac14
maic-apps <yckntjj6fy@privaterelay.appleid.com>
2026-09-25 17:21:30 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Lebenskompass – Offline-Speicher. Bei jeder neuen Etappe die Nummer erhöhen. */
-const CACHE = "lebenskompass-e5";
+const CACHE = "lebenskompass-e5b";
 const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
